6c76f81081a9dcee24e743077c070010d970c48e
Hayttle <hayttle#gmail.com>
2022-10-24 16:34:09 -0300

    validation and regex implemented

--- src/main.js
+++ src/main.js
@@ -29,27 +29,27 @@ const expirationDatePattern = {
 }
 const expirationDateMasked = Imask(expirationDate, expirationDatePattern)
 
-const cardNumber = document.querySelector("card-number")
+const cardNumber = document.querySelector("#card-number")
 const cardNumberPattern = {
   mask: [
     {
-      mark: "0000 0000 0000 0000",
+      mask: "0000 0000 0000 0000",
       regex: /^4\d{0,15}/,
       cardType: "visa",
     },
     {
-      mark: "0000 0000 0000 0000",
+      mask: "0000 0000 0000 0000",
       regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardType: "mastercard",
     },
     {
-      mark: "0000 0000 0000 0000",
+      mask: "0000 0000 0000 0000",
       cardType: "default",
     },
   ],
   dispatch: function (appended, dynamicMasked) {
     const number = (dynamicMasked.value + appended).replace(/\D/g, "")
-    const foundMask = dynamicMasked.compiledMasked.find(function (item) {
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
       return number.match(item.regex)
     })
     return foundMask
